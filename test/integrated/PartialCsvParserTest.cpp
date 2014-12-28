612a43c05cb2c8beee2a41600e40637f45d75f1c
nakatani.sho <nakatani.sho@dena.jp>
2014-12-29 02:43:24 +0900

    works with CSV without header line

--- test/integrated/PartialCsvParserTest.cpp
+++ test/integrated/PartialCsvParserTest.cpp
@@ -67,3 +67,27 @@ TEST_F(PartialCsvParserTest, 1worker_WithHeader_2col_3line_WithoutQuote_WithoutL
 
   EXPECT_EQ(PartialCsvParser::NO_MORE_ROW, parser.get_row());
 }
+
+TEST_F(PartialCsvParserTest, 1worker_WithoutHeader_2col_3line_WithoutQuote_WithLastNL) {
+  CsvConfig csv_config("fixture/WithoutHeader_2col_3line_WithoutQuote_WithLastNL.csv", false);
+  PartialCsvParser parser(csv_config);
+
+  std::vector<std::string> row;
+
+  EXPECT_NE(PartialCsvParser::NO_MORE_ROW, row = parser.get_row());
+  EXPECT_EQ(2, row.size());
+  EXPECT_EQ("101", row[0]);
+  EXPECT_EQ("102", row[1]);
+
+  EXPECT_NE(PartialCsvParser::NO_MORE_ROW, row = parser.get_row());
+  EXPECT_EQ(2, row.size());
+  EXPECT_EQ("201", row[0]);
+  EXPECT_EQ("202", row[1]);
+
+  EXPECT_NE(PartialCsvParser::NO_MORE_ROW, row = parser.get_row());
+  EXPECT_EQ(2, row.size());
+  EXPECT_EQ("301", row[0]);
+  EXPECT_EQ("302", row[1]);
+
+  EXPECT_EQ(PartialCsvParser::NO_MORE_ROW, parser.get_row());
+}
