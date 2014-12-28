9ba7d525631af7b6c03f8820f74a199e7519e124
nakatani.sho <nakatani.sho@dena.jp>
2014-12-29 02:40:24 +0900

    works even without last NL

--- test/integrated/PartialCsvParserTest.cpp
+++ test/integrated/PartialCsvParserTest.cpp
@@ -18,7 +18,34 @@ TEST_F(PartialCsvParserTest, 1worker_WithHeader_2col_3line_WithoutQuote_WithLast
   EXPECT_EQ("col1", headers[0]);
   EXPECT_EQ("col2", headers[1]);
 
-  //PartialCsvParser parser(csv_config, csv_config.body_offset(), csv_config.filesize());
+  PartialCsvParser parser(csv_config);
+
+  std::vector<std::string> row;
+
+  EXPECT_NE(PartialCsvParser::NO_MORE_ROW, row = parser.get_row());
+  EXPECT_EQ(2, row.size());
+  EXPECT_EQ("101", row[0]);
+  EXPECT_EQ("102", row[1]);
+
+  EXPECT_NE(PartialCsvParser::NO_MORE_ROW, row = parser.get_row());
+  EXPECT_EQ(2, row.size());
+  EXPECT_EQ("201", row[0]);
+  EXPECT_EQ("202", row[1]);
+
+  EXPECT_NE(PartialCsvParser::NO_MORE_ROW, row = parser.get_row());
+  EXPECT_EQ(2, row.size());
+  EXPECT_EQ("301", row[0]);
+  EXPECT_EQ("302", row[1]);
+
+  EXPECT_EQ(PartialCsvParser::NO_MORE_ROW, parser.get_row());
+}
+
+TEST_F(PartialCsvParserTest, 1worker_WithHeader_2col_3line_WithoutQuote_WithoutLastNL) {
+  CsvConfig csv_config("fixture/WithHeader_2col_3line_WithoutQuote_WithoutLastNL.csv");
+  std::vector<std::string> headers = csv_config.headers();
+  EXPECT_EQ("col1", headers[0]);
+  EXPECT_EQ("col2", headers[1]);
+
   PartialCsvParser parser(csv_config);
 
   std::vector<std::string> row;
