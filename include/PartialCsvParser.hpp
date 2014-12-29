95df34915f570d6e9defdd6240a60fb52e518d71
nakatani.sho <nakatani.sho@dena.jp>
2014-12-29 13:16:33 +0900

    Parses first line in CsvConfig whether or not header line exists

--- include/PartialCsvParser.hpp
+++ include/PartialCsvParser.hpp
@@ -92,10 +92,9 @@ public:
  *
  * CSV format is (weakly) defined in <a href="http://tools.ietf.org/html/rfc4180#section-2">RFC4180 Section2</a>
  *
- * PartialCsvParser throws this exception when one of the following conditions is satisfied.
+ * PartialCsvParser throws this exception when the following condition is satisfied.
  * @li A row has different number of columns from first row.
- * @li File has 2 ore more empty line at the end.
- */
+  */
 class PCPCsvError : public PCPError {
 public:
   PCPCsvError(const std::string &cause)
@@ -210,8 +209,7 @@ public:
   : filepath(filepath), has_header_line(has_header_line),
     field_terminator(field_terminator), line_terminator(line_terminator),
     enclosure_char(enclosure_char),
-    header_length(CsvConfig::HEADER_LENGTH_NOT_CALCULATED),
-    n_columns(CsvConfig::N_COLUMNS_NOT_CALCULATED)
+    n_columns(0)
   {
     if ((fd = open(filepath, O_RDONLY)) == -1)
       STRERROR_THROW(PCPError, std::string("while open ") + filepath);
@@ -219,11 +217,18 @@ public:
     if ((csv_text = static_cast<const char *>(mmap(NULL, csv_size, PROT_READ, MAP_PRIVATE, fd, 0))) == (void*)-1)
       STRERROR_THROW(PCPError, std::string("while mmap ") + filepath);
 
-    // calculate number of columns in first row.
+    // parse first line to calculate n_columns
     const char * line = 0;
-    size_t length = 0;
-    _get_current_line(csv_text, csv_size, 0, line_terminator, &line, &length);
-    n_columns = _split(line, length, field_terminator).size();
+    size_t line_length = 0;
+    _get_current_line(csv_text, csv_size, 0, line_terminator, &line, &line_length);
+    std::vector<std::string> columns = _split(line, line_length, field_terminator);
+    n_columns = columns.size();
+
+    // set headers if exist
+    if (has_header_line) {
+      header_length = line_length;
+      headers = columns;
+    }
   }
 
   ~CsvConfig() {
@@ -249,22 +254,18 @@ public:
   /**
    * Return the offset where CSV body (excluding header line) starts from.
    */
-  inline size_t body_offset() {
+  inline size_t body_offset() const {
     if (!has_header_line) return 0;
-    if (header_length != HEADER_LENGTH_NOT_CALCULATED) return header_length + 1;
-    headers();  // calculate header_length
-    return header_length;
+    return header_length + 1;
   }
 
   /**
    * Return header string array.
    * \p has_header_line flag must be set true in constructor.
    */
-  inline std::vector<std::string> headers() {
+  inline std::vector<std::string> get_headers() const {
     ASSERT(has_header_line);
-    const char * line = 0;
-    _get_current_line(csv_text, csv_size, 0, line_terminator, &line, &header_length);
-    return _split(line, header_length, field_terminator);
+    return headers;
   }
 
   /**
@@ -291,11 +292,10 @@ private:
   size_t csv_size;
   const char * csv_text;
 
+  std::vector<std::string> headers;
   size_t header_length;
-  static const size_t HEADER_LENGTH_NOT_CALCULATED = -1;
 
   size_t n_columns;
-  static const size_t N_COLUMNS_NOT_CALCULATED = -1;
 
   PREVENT_CLASS_DEFAULT_METHODS(CsvConfig);
 };
@@ -309,7 +309,7 @@ public:
    * @param csv_config Instance of CsvConfig.
    * @param parse_from CSV file's <em>approximate</em> offset to start parsing. Must be no less than CsvConfig::body_offset().
    *   \p parse_from = PARSE_FROM_BODY_BEGINNING has the same meaning with \p parse_from = CsvConfig::body_offset().
-   * @param parse_to CSV file's <em>approximate</em> offset to stop parsing. Must be greater than \p parse_from and less than CsvConfig::filesize().
+   * @param parse_to CSV file's <em>approximate</em> offset to stop parsing. Must be no less than \p parse_from and less than CsvConfig::filesize().
    *   \p parse_to = PARSE_TO_FILE_END has the same meaning with \p parse_to = CsvConfig::filesize() - 1.
    *
    * In order to fully parse CSV lines without overlaps, \p parse_from and \p parse_to are interpreted with the following strategy.
@@ -353,7 +353,7 @@ public:
     if (parse_to == PARSE_TO_FILE_END) this->parse_to = csv_config.filesize() - 1;
     cur_pos = this->parse_from;
     ASSERT(csv_config.body_offset() <= this->parse_from);
-    ASSERT(this->parse_from < this->parse_to);
+    ASSERT(this->parse_from <= this->parse_to);
     ASSERT(this->parse_to < csv_config.filesize());
   }
 
