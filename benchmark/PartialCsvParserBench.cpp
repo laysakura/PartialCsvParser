d3ec8e79aa5ba722a3b5bd14778e8f001e57a9a3
nakatani.sho <nakatani.sho@dena.jp>
2014-12-30 01:43:29 +0900

    no double-free anymore

--- benchmark/PartialCsvParserBench.cpp
+++ benchmark/PartialCsvParserBench.cpp
@@ -5,34 +5,36 @@
 #include <iostream>
 #include <pthread.h>
 
-static std::unordered_map<pthread_t, size_t> n_columns;
+typedef struct parser_thread_arg_t {
+  PCP::partial_csv_t partial_csv;
+  size_t n_columns;
+} parser_thread_arg_t;
 
-void * partial_parse(PCP::partial_csv_t * partial_csv) {
-  // initialize counter
-  pthread_t tid = pthread_self();
-  n_columns[tid] = 0;
-
+void * partial_parse(parser_thread_arg_t * arg) {
   // instantiate parser
-  PCP::PartialCsvParser parser(*partial_csv->csv_config, partial_csv->parse_from, partial_csv->parse_to);
+  PCP::PartialCsvParser parser(*arg->partial_csv.csv_config, arg->partial_csv.parse_from, arg->partial_csv.parse_to);
 
   // parse & count-up columns
   std::vector<std::string> row;
-  while (!(row = parser.get_row()).empty()) n_columns[tid] += row.size();
+  while (!(row = parser.get_row()).empty()) arg->n_columns += row.size();
 
   return NULL;
 }
 
 int main() {
   const char * filepath = "in.csv";
-  const size_t n_threads = 1;
+  const size_t n_threads = 4;
 
   PCP::CsvConfig csv_config(filepath, false);
 
   // Setup range each thread parse.
   size_t size_per_thread = (csv_config.filesize() - csv_config.body_offset()) / n_threads;
-  std::vector<PCP::partial_csv_t> partial_csvs(n_threads);
+  std::vector<parser_thread_arg_t> parser_thread_args(n_threads);
   for (size_t i = 0; i < n_threads; ++i) {
-    PCP::partial_csv_t & partial_csv = partial_csvs[i];
+    parser_thread_arg_t & parser_thread_arg = parser_thread_args[i];
+    parser_thread_arg.n_columns = 0;
+
+    PCP::partial_csv_t & partial_csv = parser_thread_arg.partial_csv;
     partial_csv.csv_config = &csv_config;
     partial_csv.parse_from = csv_config.body_offset() + i * size_per_thread;
     partial_csv.parse_to = csv_config.body_offset() + (i + 1) * size_per_thread - 1;
@@ -41,14 +43,14 @@ int main() {
   // create threads
   std::vector<pthread_t> tids(n_threads);
   for (size_t i = 0; i < n_threads; ++i)
-      pthread_create(&tids[i], NULL, (void *(*)(void *))partial_parse, &partial_csvs[i]);
+      pthread_create(&tids[i], NULL, (void *(*)(void *))partial_parse, &parser_thread_args[i]);
   // join threads
   for (size_t i = 0; i < n_threads; ++i)
       pthread_join(tids[i], NULL);
 
   // calculate total number of columns
   size_t n_total_columns = 0;
-  for (size_t i = 0; i < n_threads; ++i) n_total_columns += n_columns[tids[i]];
+  for (size_t i = 0; i < n_threads; ++i) n_total_columns += parser_thread_args[i].n_columns;
 
   std::cout << n_total_columns << std::endl;
 
